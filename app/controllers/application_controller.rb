c4d34a40bb34ba083a3c61c2f004a521c1319120
k saebdal <ksaebdal@kristian-saebdals-macbook-pro.local>
2010-10-26 22:12:20 +0200

    a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,12 +19,11 @@ class ApplicationController < ActionController::Base
     doc.xpath('//tr//td[@class = "wr"]|//tr//td[@class = "wrnew"]').each do |wrs|
       if element % 3 == 0
         level = wrs.text
-        #        puts level.slice(3..-1)
       elsif element % 3 == 1
         time = wrs.text
       elsif element % 3 == 2
         name = wrs.text
-        wrs_table[level.slice(0..1)] = {name,time}
+        wrs_table[level.slice(0..1)] = [name,time]
       else
         #throw fail
       end 
@@ -44,33 +43,17 @@ class ApplicationController < ActionController::Base
       element = 0
       allset = 0
       #doc.xpath('//td[@class = "list"][@width = 100]|//td[@class = "list"][@width = 22]|//td[@class = "list"]//a').each do |wrs|
-      doc.xpath('//td[@class = "list"][@width = 100]|//td[@class = "list"][@width = 22]|//td[@class = "list"]//a').each do |wrs|
-        if element % 4 == 0
-          level = wrs.text
-          allset += 1
-          #        puts level.slice(3..-1)
-        elsif element % 4 == 1
-          name = wrs.text
-          allset += 1
-        elsif element % 4 == 2
-          team = wrs.text
-        elsif element % 4 == 3
-          time = wrs.text
-          allset += 1
-        else
-          #throw fail
-        end 
-        if allset == 3
-          allset = 0
-          puts level.slice(0..1)
-          puts name
-          puts team
-          puts time
-          wrs_table[level.slice(0..1)] = {name + " " + team,time}
-        end  
-        element += 1
-        puts wrs
+      times = doc.xpath('//td[@class = "list"][@width = 100]')
+      puts times[1].text
+      names_and_teams = doc.xpath('//td[@class = "list"]//a')
+      puts "list"
+      puts names_and_teams[1].text
+      #puts names_and_teams.size
+      #puts names_and_teams.to_s
+      if (names_and_teams[1].text =~ /\[.?\]/)
+        puts "ok"
       end
+      #<a href="http://elmaonline.moposite.com/?s=players&amp;t=EPO">[EPO]</a>
       wrs_table
   end
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,13 +4,27 @@ require 'nokogiri'
 class HomeController < ApplicationController
 
   def index
-  #  single_table = single_wrs
-  #  single_table.each {|key, value| puts "#{key} is #{value}" }
-    single_table = eol_single_wrs
-    #single_table.each {|key, value| puts "#{key} is #{value}" }
-
+    single_table = single_wrs()
+    millis = calculate_total(single_table)
+    @total = format_millis(millis)
   end
 
-  
-  
+  def format_millis(millis)
+    minutes = (millis/60000).to_i
+    seconds = ((millis-minutes*60000)/1000).to_i
+    houndreds = ((millis-minutes*60000-seconds*1000)/10).to_i
+    "#{minutes.to_s.rjust(2, '0')}:#{seconds.to_s.rjust(2, '0')},#{houndreds.to_s.rjust(2, '0')}"
+  end
+
+  def calculate_total(table)
+    total = 0
+    table.each do |key, value|
+      if value[1] =~ /([0-9]*)\:*([0-9][0-9]),([0-9][0-9])/
+        millis = ($3.to_i*10) + ($2.to_i*1000) + ($1.to_i*60*1000)
+        total = total + millis
+      end
+    end
+    total
+  end
+
 end
